19065c86e47f6f68eb25c6052de0f42897c7abc7
Ben bai <benyebai@gmail.com>
2021-11-07 00:18:50 -0700

    j

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { Whiteblack } from './whiteblack/whiteblack';
 import {AllTasks} from "./taskStuff/allTasks"
 import Container from './whiteBoard/Container';
 
-<<<<<<< HEAD
 
 export class App extends React.Component {
   constructor(props) {
@@ -42,55 +41,5 @@ export class App extends React.Component {
 
       
     </div>
-=======
-var gamer;
-/*
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    gamer = request.greeting;
-    console.log("BEfore");
-    console.log(gamer);
-    console.log("AFTER");
-    
-    if (request.greeting !== "hello")
-
-
-      sendResponse({farewell: "ff"});
-  }
-);
-*/
-
-
-export class App extends React.Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      which : "tasks"
-    };
-  }
-
-  render(){
-    let toDisplay;
-    if(this.state.which == "tasks"){
-      toDisplay = <AllTasks />;
-    }
-    else{
-      toDisplay = <Whiteblack />;
-    }
-
-    return (
-      <div className="App">
-        <div className = "topBar">
-          <button onClick={() => {this.setState({"which" : "tasks"})}}>tasks</button>
-          <button onClick={() => {this.setState({"which" : "whiteBlack"})}}>whiteblack</button>
-        </div>
-        {gamer}
-
-        {toDisplay}
-      </div>
-
->>>>>>> c2f07fe5b5bd803f900e4434791073c4e272a66a
-    );
-  }
-}
+  )
+  }}
